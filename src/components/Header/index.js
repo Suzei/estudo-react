d613a74baf800efdb168262352f74f1eb9a99f33
Suzei <odetofack@gmail.com>
2021-03-19 06:11:13 -0300

    Repository + Header finished

---
+++ src/components/Header/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './style.css'
+import 'aos/dist/aos.css'
+import Aos from 'aos';
+
+
+Aos.init( {
+    duration: 2000
+}
+    
+);
+
+function Header() {
+    return (
+        <header>
+            <nav className="nav">
+                
+                <a href="">Home</a>
+                <a href="">Documentation</a>
+                <a href="">Versions</a>
+                <a href="">React.Js</a>
+                <a href="">Plans</a>
+                <a href="">Support</a>
+                <a href="">Sign Up</a>
+            </nav>
+
+            <section className="title-hero">
+                <section className="img-container">
+                   <div className="img"></div>
+                </section>
+
+                <div className="title" data-aos="fade-left">
+                    <h1>REACT PURPLE</h1>
+                    <p>Expanda sua experiência react com todos os benefícios de ter uma plataforma 100% feita para desenvolvimento</p>
+                    <button>Download</button>
+                </div>
+            </section>
+
+            
+        </header>
+    );
+}
+
+export default Header;
